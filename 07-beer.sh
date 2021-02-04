9b10b58bf5982beecf1f6d351b5802610ad90270
nporter1 <nporter1@ggc.edu>
2021-02-04 15:46:10 -0500

    Nick Porter committing 07-beer.sh to the NickP branch... at least I hope so

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,23 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+
+echo "Howdy! You made me drink a butt-ton of beer, and I'd like to do the same with Alka-seltzer!"
+echo "How much Alka-seltzer should I take?"
+read countA
+
+while [ $countA -ge 0 ]; do
+  if [ $countA -ge 2 ]; then
+      echo "Just took an Alka-Seltzer and passed another one down, $countA left"
+  elif [ $countA -eq 1 ]; then
+      echo "Just took one Alka-Seltzer but no more to pass down, $countA left"
+  else
+      echo "no more bottles of beer on the wall"
+  fi
+  
+  # the following statement is equivalent to: let "count=count-1"
+  ((countA = countA - 1))
+
+done
+
+echo "Whew, I feel less drunk now, thanks!"
