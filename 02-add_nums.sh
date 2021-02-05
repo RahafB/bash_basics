231b586eb8ef361e9dce9dde13e089d0b861e343
Keyvan Shabani <kshabani@ggc.edu>
2021-02-05 10:07:05 -0500

    finish exercise for this file

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,11 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "**********Second Branch*************"
+echo "What is the width of your screen?"
+read width
+echo "What is the height of your screen?"
+read height
+
+total=$(( width*height))
+echo "You have $total number of pixels "
