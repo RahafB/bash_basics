7a30d49f96b37272c960fd6267e2166c15b769e7
Greatzel Unabia <lunabia@ggc.edu>
2021-02-05 19:53:26 -0500

    edited the add numbers file. increased from 3 to 4 numbers.

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -6,10 +6,13 @@ echo "Enter your second number"
 read second
 echo "Enter your third number"
 read third
+echo "Enter your 4th number"
+read fourth
 
-sum=$(( first+second+third ))
+sum=$(( first+second+third+fourth ))
 
 echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+# Added a 4th digit to the sums... Greatzel
