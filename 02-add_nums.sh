b60d302acb4ff8570ea39ee4263576a3fcba97be
AyeshaSyeda <asyeda@ggc.edu>
2021-02-05 22:19:03 -0500

    Made a parallel universe and modified 02-add_nums.sh

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -11,5 +11,14 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
+echo "Enter length:"
+read length
+echo "Enter width:"
+read width
+
+product=$(( length*width))
+
+echo "The total number of pixels is $product"
+
 # exercise: ask the user for the width and height and present total
 # number of pixels
