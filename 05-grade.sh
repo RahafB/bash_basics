81efab14269b5f7431cc23cecc82440db37ba679
Long Phan <blong077.2018@gmail.com>
2021-08-27 19:28:00 -0400

    Add scripting in 05-grade for git assignment 2

--- 05-grade.sh
+++ 05-grade.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
 
 
-echo "What did you get in the first ITEC 3860 test?"
-echo "Please give a numeric answer"
-read grade
+# echo "What did you get in the first ITEC 3860 test?"
+# echo "Please give a numeric answer"
+# read grade
 
-if [ $grade -ge 90 ]; then
-   echo "You got an A. Nice."
-elif [ $grade -ge 80 ]; then
-   echo "You got a B. That's good."
-elif [ $grade -ge 70 ]; then
-   echo "You got a C. Not bad."
-else
-   echo "Time to work on some extra credit assignments"
-fi
+# if [ $grade -ge 90 ]; then
+#    echo "You got an A. Nice."
+# elif [ $grade -ge 80 ]; then
+#    echo "You got a B. That's good."
+# elif [ $grade -ge 70 ]; then
+#    echo "You got a C. Not bad."
+# else
+#    echo "Time to work on some extra credit assignments"
+# fi
 
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+
+echo "What is the temprature in Boston?"
+read temprature
+
+if [ $temprature -lt 40 ]; then
+	echo "It's cold."
+elif [ $temprature -lt 60 ]; then
+	echo "It's chilly."
+elif [ $temprature -lt 70 ]; then
+	echo "It's okay."
+else
+	echo "It's hot."
+
+fi
