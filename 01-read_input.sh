f4b839a53d4557c24202395694fa291486ee7dea
Ruby Hernandez <rhernandez10@ggc.edu>
2021-08-29 19:53:36 -0400

    Made changes, again, to file

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -4,9 +4,12 @@
 echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
-
-# single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
+
+echo "Enter a filename"
+read varfilename
+touch $varfilename
+echo "You have created $varfilename"
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
