444132cd57e18abad2ee0b76d899b9097b2f76f7
Ruby Hernandez <rhernandez10@ggc.edu>
2021-08-29 19:45:29 -0400

    Made changes to file

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -4,9 +4,13 @@
 echo "Hello! What is your name"
 read name
 echo "Welcome, $name"
-
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
+
+echo "Enter a filename to be created (include the file extension)"
+read filename
+touch $filename
+echo "$filename has been created"
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
