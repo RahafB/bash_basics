eef9d00cdfb1ed1c7f28d6100b4065c7122105d0
Michelle Watson <michellewatson0202@gmail.com>
2021-08-30 14:21:41 -0400

    added questions asking if its the weekend

--- 03-happy.sh
+++ 03-happy.sh
@@ -14,3 +14,12 @@ fi
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+  
+  echo "is today the weekend?"
+  read answer
+
+  if [ "$answer" = "yes" ]; then
+     echo "YAY!"
+  else
+     echo "BOOHOO!"
+  fi
