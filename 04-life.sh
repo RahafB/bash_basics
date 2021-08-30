27c57b12e911ec680c61e39d464365a8cc650b97
Michelle Watson <michellewatson0202@gmail.com>
2021-08-30 14:33:25 -0400

    added to life file

--- 04-life.sh
+++ 04-life.sh
@@ -12,5 +12,15 @@ fi
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
-# exercise: write a script that prints whether it is 
+# exercise: write a script that prints whether it is
 # morning or not
+
+echo "is it morning"
+read isMorning
+
+
+if [ "$isMorning" -eq 1 ]; then
+   echo "Yes!, it is morning!"
+else
+   echo "Awww... the day has passed you by"
+fi
