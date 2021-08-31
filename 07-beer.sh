f416ab2c5759a5e92167c12a668a91183f0514fa
Constance Yang <cyang7@ggc.edu>
2021-08-30 21:42:32 -0400

    changed the beer song but parallel

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,27 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+
+echo " "
+
+echo "Time to do the exercise! Let's eat some jelly beans!"
+echo "How many jelly beans do we have?"
+read countBeans
+while [ $countBeans -ge 0 ]; 
+do
+  if [ $countBeans -ge 2 ]; 
+  then
+      echo "$countBeans jelly beans are in your hand, you eat 1."
+      echo "Let's keep eating."
+  elif [ $countBeans -eq 1 ]; 
+  then
+      echo "$countBeans jelly bean is in your hand, eat the last one."
+      echo "Let's see now..."
+  else
+      echo "No more beans are left."
+  fi
+  
+  ((countBeans = countBeans - 1))
+  #i ran out of unique songs
+
+done 
