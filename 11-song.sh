5a027fb40de6815f21b68df3212d76cb19ab943f
Turner <tnalley@ggc.edu>
2021-09-16 17:39:12 -0400

    changed 11_song the same way but there is aditioal letters added

--- 11-song.sh
+++ 11-song.sh
@@ -1,36 +1,74 @@
 #!/bin/sh
 
+###
+#while [ true ]; do
+
+   # you can show a prompt with the read command
+#   read -p "Do rae mi fa so la ti do? (q to end) > " note
+
+#case $note in
+   # each case matches a pattern
+#   do|Do)
+#      echo "Doe a deer a female deer"
+#      ;;   
+#   rae|Rae)
+#      echo "Ray a drop of golden sun"
+#      ;;
+#   mi|Mi)
+#      echo "Me a name a call myself"
+#      ;;
+#   fa|Fa)
+#      echo "Far a long long way to run"
+#      ;;
+#   so|So)
+#      echo "So a note that follows fa"
+#      ;;
+#   la|La)
+#      echo "La a note that follow so"
+#      ;;
+#   ti|ta)
+#      echo "Tea I drink with jam and bread"
+#      ;;
+#   q)
+#      echo "Hope you enjoyed the sound of music"
+#      exit 0
+#      ;;
+#   *) 
+#      echo "Not a note"
+#      ;;
+#esac
+
+#done
+###
+
+# exercise: Change the input question and case 
+# statement to another theme. 
 
 while [ true ]; do
 
    # you can show a prompt with the read command
-   read -p "Do rae mi fa so la ti do? (q to end) > " note
+   read -p "A G I L E? (q to end) > " notes
 
 case $note in
    # each case matches a pattern
-   do|Do)
-      echo "Doe a deer a female deer"
+   a|A)
+      echo "Advanced a"
       ;;   
-   rae|Rae)
-      echo "Ray a drop of golden sun"
+   g|G)
+      echo "Generation g"
       ;;
-   mi|Mi)
-      echo "Me a name a call myself"
+   i|I)
+      echo "Interlopability I"
       ;;
-   fa|Fa)
-      echo "Far a long long way to run"
+   l|L)
+      echo "Law l"
       ;;
-   so|So)
-      echo "So a note that follows fa"
+   e|E)
+      echo "Enforcement e"
       ;;
-   la|La)
-      echo "La a note that follow so"
-      ;;
-   ti|ta)
-      echo "Tea I drink with jam and bread"
-      ;;
+
    q)
-      echo "Hope you enjoyed the sound of music"
+      echo "Wrong Agile..... again"
       exit 0
       ;;
    *) 
@@ -39,6 +77,3 @@ case $note in
 esac
 
 done
-
-# exercise: Change the input question and case 
-# statement to another theme. 
