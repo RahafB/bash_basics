05af13501477772acb26d86db93e0130780ea313
MarshMaster11 <66848119+MarshMaster11@users.noreply.github.com>
2022-01-28 11:19:45 -0500

    Modify add_nums

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,15 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Please enter the picture's height"
+read height
+echo "Please enter the picture's width"
+read width
+
+echo "Calculating the total pixels in the picture..."
+
+px=$(( height * width ))
+echo "Calculation complete"
+echo "Your picture has a total of $px pixels"
+
