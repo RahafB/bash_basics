1ea8efbdec83eaa3536108a771321ccde84a9861
Chris Hernandez <chernandez21@ggc.edu>
2022-01-31 23:19:29 -0500

    finished the exercise to asks the user for a filename

---
+++ readInput.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! my name is Chris Hernandez!"
+
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+echo "Hello, type in the name of a new file to create"
+read filename
+touch $filename
+
+
