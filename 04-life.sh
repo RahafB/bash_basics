f997c5568f58e3ac69655101ed6d7c8512f32586
Anthony <atran5@ggc.edu>
2022-02-02 18:41:47 -0500

    practice

--- 04-life.sh
+++ 04-life.sh
@@ -4,9 +4,9 @@
 h=$(date +"%H")
 if [ $h -gt 6 -a $h -le 12 ]
 then
-echo it is morning
+echo it is morning!
 else
-echo it is not morning
+echo it is not morning!
 fi
 
 #  here are some other arithemetic comparison operators
