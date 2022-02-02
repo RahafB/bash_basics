b7fcfa4e319e7c481ff75a3a13af61b00ecc4ebe
yariscq <yariscq@gmail.com>
2022-02-01 19:30:17 -0500

    commit

--- 07-beer.sh
+++ 07-beer.sh
@@ -17,8 +17,27 @@ while [ $count -ge 0 ]; do
   
   # the following statement is equivalent to: let "count=count-1"
   ((count = count - 1))
+ done
+echo "How many days until Christmas"
+
+read days
+
+while [ $days -ge 0 ]; do
+  if [ $days -ge 2 ]; then
+      echo "on the $days day of Christmas, my true love gave to me...."
+      echo "something something and a pear tree"
+  elif [ $days -eq 1 ]; then
+      echo "on the last day of Christmas, my true love gave to me"
+      echo "a chocolate and a rosey"
+  else
+      echo "Christmas is over"
+  fi
+  
+  # the following statement is equivalent to: let "count=count-1"
+  ((days = days - 1))
 
 done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+# nothing
