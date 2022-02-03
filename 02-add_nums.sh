f9267456179c5f2fbcb3ba5c218b2fb7c70acbe0
Chris Hernandez <chernandez21@ggc.edu>
2022-02-02 21:44:28 -0500

    changed script to find the area using height and width

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-echo "Enter your first number"
-read first
-echo "Enter your second number"
-read second
-echo "Enter your third number"
-read third
+#echo "Enter your first number"
+#read first
+#echo "Enter your second number"
+#read second
+#echo "Enter your third number"
+#read third
 
-sum=$(( first+second+third ))
+#sum=$(( first+second+third ))
 
-echo "The sum is $sum"
+#echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Enter your width of your rectangle"
+read first
+echo "Enter the height of your rectangle"
+read second
+
+product=$((first * second))
+echo "The area of your rectangle is $product pixels"
