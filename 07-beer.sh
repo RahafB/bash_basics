da31d479950d655635590f9d57388cb42ed6f7ce
yariscq <yariscq@gmail.com>
2022-02-02 19:09:58 -0500

    please errorr
    pleeeaasse

--- 07-beer.sh
+++ 07-beer.sh
@@ -39,7 +39,9 @@ while [ $daysss -ge 0 ]; do
   
   # the following statement is equivalent to: let "count=count-1"
   ((daysss = daysss - 1))
-
+done
+done 
+done
 
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
