1603c4514d4e9f3aa9202923a3b6b4d47338ffad
Chris Hernandez <chernandez21@ggc.edu>
2022-02-02 22:15:33 -0500

    edited the script on a different branch

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-echo "Enter your first number"
-read first
-echo "Enter your second number"
-read second
-echo "Enter your third number"
-read third
+#echo "Enter your first number"
+#read first
+#echo "Enter your second number"
+#read second
+#echo "Enter your third number"
+#read third
 
-sum=$(( first+second+third ))
+#sum=$(( first+second+third ))
 
-echo "The sum is $sum"
+#echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "this is the 'parallel universe' or the other branch"
+
+echo "Enter the width of your rectangle"
+read first
+echo "Enter the height of your rectangle"
+read second
+
+area=$((first * second))
+
+echo "The area is $area pixels"
+
+
+
