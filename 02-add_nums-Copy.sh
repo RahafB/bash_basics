721d08b762e337768860cf5435b3edabdaff9395
Ti Nguyen <tnguyen130@ggc.edu>
2022-02-02 23:10:01 -0500

    exercise 2

---
+++ 02-add_nums-Copy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Enter your first number"
+read first
+echo "Enter your second number"
+read second
+echo "Enter your third number"
+read third
+
+sum=$(( first+second+third ))
+
+echo "The sum is $sum"
+
+# exercise: ask the user for the width and height and present total
+# number of pixels
+
+echo "Enter width"
+read width
+echo "Enter height"
+read height
+
+total=$(( width*height))
+
+echo "The total number of pixels is $total"
