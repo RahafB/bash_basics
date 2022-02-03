1b2c272c34fdae0105d94c8e90c8fc376b463813
wilmer <wilmer_will@outlook.com>
2022-02-02 20:54:59 -0500

    Created weather script

--- 05-grade.sh
+++ 05-grade.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
-
-
-echo "What did you get in the first ITEC 3860 test?"
-echo "Please give a numeric answer"
-read grade
-
-if [ $grade -ge 90 ]; then
-   echo "You got an A. Nice."
-elif [ $grade -ge 80 ]; then
-   echo "You got a B. That's good."
-elif [ $grade -ge 70 ]; then
-   echo "You got a C. Not bad."
-else
-   echo "Time to work on some extra credit assignments"
-fi
-
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+echo "What is the current weather"
+echo "Please give a numeric answer"
+read grade
+
+if [ $grade -le 40 ]; then
+   echo "It's cold"
+elif [ $grade -le 60 ]; then
+   echo "It's chilly"
+elif [ $grade -le 70 ]; then
+   echo "It's okay"
+else
+   echo "It's hot"
+fi
