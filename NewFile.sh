009eea330de60cd31411bfc3dc06329aa14a3c95
Kelan <kelandesu@gmail.com>
2022-08-18 19:55:19 -0400

    your change meddage

---
+++ NewFile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+echo "Enter filename"
+read filename
+touch $filename.txt
+echo "The file $filename has been created"
