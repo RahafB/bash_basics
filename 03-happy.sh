686487a2cd59d980128a58554a1431ed743a22cd
Saif <sshaikh4@ggc.edu>
2022-09-02 18:40:41 -0400

    I add a few new lines to file 03-happy.sh

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo "You are happy?"
+echo "You are happy? Yes, N0, Soso"
 read answer
 
 if [ "$answer" = "yes" ]; then
    echo "hmmmm gooood yess you are happy"
+elif [ "$answer" = "Soso" ]; then
+   echo "You have mixed feelings"
 else
    echo "Still Smile c:"
 fi
