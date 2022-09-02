7a1467ee12e7710a01b4d2ceed77b3e79641a394
Chase Harrod <charrod@ggc.edu>
2022-09-02 11:16:59 -0400

    adds exercise commands which prompt the user to enter screen height and width in pixels and presents the total number of pixels

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,12 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+
+echo "Enter your screen width in pixels"
+read width
+echo "Enter your screen height in pixels"
+read height
+
+totalPixels=$((width*height))
+
+echo "The total number of pixels on your screen is $totalPixels"
