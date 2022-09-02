075518ac6e16a60b225d05076a89f1ba7c2b2cb9
bpemb <bpemberton@ggc.edu>
2022-09-02 19:16:52 -0400

    Added one change to Life sh

--- 04-life.sh
+++ 04-life.sh
@@ -14,3 +14,13 @@ fi
 
 # exercise: write a script that prints whether it is 
 # morning or not
+
+
+echo "Is it Morning?"
+read answer
+
+if [ "$answer" = "yes" ]; then
+   echo "Good Morning!"
+else
+   echo "Oh Well."
+fi
