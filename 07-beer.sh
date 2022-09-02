6a81f1c464d90d17720379badea9b5c7b52ba6b9
Alan Oliver <alanoli99@gmail.com>
2022-09-01 21:19:16 -0400

    Completed the exercise in the comment section for 07-beer.sh step 7

--- 07-beer.sh
+++ 07-beer.sh
@@ -22,3 +22,28 @@ done
 
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
+
+
+echo "Let's sing Little Monkeys Jumping on the Bed!"
+echo "How many Monkeys?"
+read count
+
+while [ $count -ge 0 ]; do
+  if [ $count -ge 2 ]; then
+      echo "$count  Big monkeys jumping on the bed"
+      echo "One fell off and bump his head Papa called the doc
+	  and the doc said No more monkeys jumping on the bed"
+  elif [ $count -eq 1 ]; then
+      echo "$count  Big monkeys jumping on the bed"
+      echo "One fell off and bump his head Papa called the doc
+	  and the doc said No more monkeys jumping on the bed"
+  else
+      echo "No big monkeys jumping on the bed None fell off and
+	  bumps their head Papa called the doc and the doc said 
+	  No more monkeys jumping on the bed"
+  fi
+
+  # the following statement is equivalent to: let "count=count-1"
+  ((count = count - 1))
+
+done
