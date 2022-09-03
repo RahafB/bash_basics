a13cdce3a9539ee649ea0a2e76081a13f695d867
bayannarslan <106767817+bayannarslan@users.noreply.github.com>
2022-09-02 21:07:11 -0400

    changed to letters

--- 11-song.sh
+++ 11-song.sh
@@ -4,41 +4,41 @@
 while [ true ]; do
 
    # you can show a prompt with the read command
-   read -p "Do rae mi fa so la ti do? (q to end) > " note
+   read -p "A B C D E F G H? (q to end) > " note
 
 case $note in
    # each case matches a pattern
-   do|Do)
-      echo "Doe a deer a female deer"
-      ;;   
-   rae|Rae)
-      echo "Ray a drop of golden sun"
+   a|A)
+      echo "A is for APPLE"
       ;;
-   mi|Mi)
-      echo "Me a name a call myself"
+   b|B)
+      echo "B is for BOY"
       ;;
-   fa|Fa)
-      echo "Far a long long way to run"
+   c|C)
+      echo "C is for CAT"
       ;;
-   so|So)
-      echo "So a note that follows fa"
+   e|E)
+      echo "E is for ELEPHANT"
       ;;
-   la|La)
-      echo "La a note that follow so"
+   f|F)
+      echo "F is for FLOWER"
       ;;
-   ti|ta)
-      echo "Tea I drink with jam and bread"
+   g|G)
+      echo "G is for GIRL"
+      ;;
+   h|H)
+      echo "H is for HER"
       ;;
    q)
-      echo "Hope you enjoyed the sound of music"
+      echo "Hope you learned your letters"
       exit 0
       ;;
-   *) 
-      echo "Not a note"
+   *)
+      echo "Not valid"
       ;;
 esac
 
 done
 
-# exercise: Change the input question and case 
-# statement to another theme. 
+# exercise: Change the input question and case
+# statement to another theme.
