321c7b994f312428b2d48488938183d0029dcc9e
Albert <albert.austiniv@gmail.com>
2023-04-01 09:07:19 -0400

    Edited happy.sh and added new script - Albert Austin IV

--- 03-happy.sh
+++ 03-happy.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-echo "You are happy?"
-read answer
+#echo "You are happy?"
+#read answer
 
-if [ "$answer" = "yes" ]; then
-   echo "hmmmm gooood yess you are happy"
-else
-   echo "Still Smile c:"
-fi
+#if [ "$answer" = "yes" ]; then
+#   echo "hmmmm gooood yess you are happy"
+#else
+#   echo "Still Smile c:"
+#fi
 
 # here are the other string comparison operators
 # != , -n (not an empty string) , -z (an empty string)
 
 # exercise: write a script that prints whether today is
 # the weekend or not
+
+echno "What day is it?"
+read answer
+
+if [ "$answer" - "Friday" ] || [ "$answer" - "Saturday" ] || [ "$answer" - "Sunday" ]; then
+	echo "It's the weekend!"
+else
+	echo "It's still the weekday"
+fi
