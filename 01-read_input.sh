eeec0bdfcf56d8c0859c23cf72b22479a04b0698
Luke Akridge <lakridge@ggc.edu>
2023-08-24 12:44:41 -0400

    Modified 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -8,5 +8,9 @@ echo "Welcome, $name"
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
+echo 'What file do you want to read?'
+read file
+touch file
+
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
