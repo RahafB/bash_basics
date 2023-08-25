eb2298b062715bc0654f713d88bfd410b6da823b
Anthony Wallace <awallace9@ggc.edu>
2023-08-25 07:57:28 -0400

    My edited version of 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,16 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+
+# Asking the user for a filename.
+echo "Enter a filename:"
+read filename
+
+# creating an enmpty file with that name
+touch $filename
+
+# print a confirmation message
+
+echo "Created an empty file name $filename
+
