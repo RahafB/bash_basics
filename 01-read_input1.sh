85a975bd71e7ca1ca8f5ab3026527f5374ef8bd8
mcelestino1 <mcelestino1@ggc.edu>
2023-08-27 10:37:13 -0400

    Modified script to print my name

--- 01-read_input1.sh
+++ 01-read_input1.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-# Print welcome message and name\
+# Print welcome message and name
 
-echo "Hello! My namme is Muketoi Celestino."
+echo "Hello! My name is Muketoi Celestino."
+
+# read name
+# echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+# echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a
+# filename and create an empty file named after it
