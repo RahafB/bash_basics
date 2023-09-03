419b4aeb108c81cd6287c5a840759da63080ef73
Shianne <slesure1@ggc.edu>
2023-09-03 16:27:27 -0400

    made another change in file in Intell j.

--- 05-grade.sh
+++ 05-grade.sh
@@ -20,7 +20,7 @@ fi
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
 
-echo "Is the temperature near you cold, chilly, okay, or hot?"
+echo "What is the temperature near you cold, chilly, okay, or hot?"
 echo "Please give a numeric answer"
 read temperature
 
