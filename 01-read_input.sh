906770837c9fc430218a2a8d7665a0cddd7d10e2
Marcello <rmelendez1@ggc.edu>
2023-09-03 13:55:39 -0400

    Update

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,15 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
+# Prompt the user to enter a filename
+read -p "Enter a filename: " filename
+
+# Check if the filename is already existing
+if [ -e "$filename" ]; then
+    echo "File '$filename' already exists."
+else
+    # Create an empty file with the specified filename
+    touch "$filename"
+    echo "File '$filename' has been created."
+fi
