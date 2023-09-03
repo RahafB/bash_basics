234d2e2c6787291743f1a4f9e092e413e29b8df4
mcelestino1 <mcelestino1@ggc.edu>
2023-09-02 20:59:28 -0400

    Added new command as instructed in comments.

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -8,5 +8,6 @@ echo "Welcome, $name"
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
 
-# exercise: write a script that asks the user for a 
+# exercise: write a script that asks the user for a
 # filename and create an empty file named after it
+touch $name
