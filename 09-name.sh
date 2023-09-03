3720bf09a5b5f50f27cccaa4609517f5bb484393
Holly Boaz <hollykaboaz@gmail.com>
2023-09-03 17:23:38 -0400

    step 3 of git assignment 2

--- 09-name.sh
+++ 09-name.sh
@@ -2,10 +2,10 @@
 
 # look up ip addresses of various search engines
 
-servers="yahoo.com google.com dogpile.com wolframalpha.com"
+servers="google.com facebook.com twitter.com instagram.com"
 
 for server in $servers; do
-    nslookup $server
+    nslookup  $server
     echo "----------------------------"
 done
 
