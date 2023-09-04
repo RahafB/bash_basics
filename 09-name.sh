170ff2f55a30f66a3eaaf5c31312a2c5c836d1c8
Ben Tacota <tbenny2000@yahoo.com>
2023-09-03 22:56:17 -0400

    name file modified

--- 09-name.sh
+++ 09-name.sh
@@ -6,7 +6,21 @@ servers="yahoo.com google.com dogpile.com wolframalpha.com"
 
 for server in $servers; do
     nslookup $server
-    echo "----------------------------"
+    echo "Server:  dsldevice6.attlocal.net
+Address:  2600:1700:60a0:a30::1
+
+Non-authoritative answer:
+Name:    google.com
+Addresses:  2607:f8b0:4023:1002::8b
+          2607:f8b0:4023:1002::66
+          2607:f8b0:4023:1002::8a
+          2607:f8b0:4023:1002::64
+          74.125.138.102
+          74.125.138.139
+          74.125.138.113
+          74.125.138.138
+          74.125.138.100
+          74.125.138.101"
 done
 
 # exercise: Change the list of servers and also the 
