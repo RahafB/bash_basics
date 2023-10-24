be8f9de0e52039fba634dd69be12d6c36779857b
krishan1298 <krishanbha129@gmail.com>
2023-10-23 22:27:14 -0400

    Edited 02-add_nums.sh in parallel branch

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,10 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "Enter new width"
+read new_width
+echo "Enter new height"
+read new_height
+area=$((new_width*new_height))
+echo "The updated number of pixels is $area"
+
