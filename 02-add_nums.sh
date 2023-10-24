448a2c203592ae974c1f86cec151ca7bd28e02f5
krishan1298 <krishanbha129@gmail.com>
2023-10-23 22:29:07 -0400

    Made changes to 02-add_nums.sh in main branch

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -13,3 +13,9 @@ echo "The sum is $sum"
 
 # exercise: ask the user for the width and height and present total
 # number of pixels
+echo "What is the height?"
+read height
+echo "What is the width?"
+read width
+area=$((height*width))
+echo "The number of pixels is $area"
