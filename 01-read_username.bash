2be2edbf610425f15269046dbbc121b7a9880de6
andrewnosa <144724951+andrewnosa@users.noreply.github.com>
2024-01-10 22:08:28 -0500

    Update and rename 01-read_input_new.sh to  01-read_username.bash

--- 01-read_username.bash
+++ 01-read_username.bash
@@ -1,12 +1,13 @@
-#!/bin/bash
+ #!/bin/bash
 # read the name of the user and print hello
 
-echo "Hello! My name is Puen Xie"
-# read name
-# echo "Welcome, $name"
+echo "Hello! What is your name"
+read Andrew
+echo "Welcome, $Andrew"
 
 # single quotes prevent the expansion of the variable
-# echo 'Your name was stored in $name'
+echo 'Your name was stored in $Andrew'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+
