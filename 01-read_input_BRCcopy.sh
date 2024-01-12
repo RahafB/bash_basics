c8cae96fb2f02b743849a2a35931c9d52940d9fb
bramoscazares <bramoscazares@gmail.com>
2024-01-11 21:53:08 -0500

    Made copy of 01-read-input.sh and created a TestFile

---
+++ 01-read_input_BRCcopy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello! What is your name"
+read name
+echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
+
+echo "I need a name for a file! What is a name I can use?"
+read filename
+touch $filename
