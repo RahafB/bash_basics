0cda4e06d37142f7e4d27b2bd15f48c942231e30
Albert <albert.austiniv@gmail.com>
2024-01-11 23:51:53 -0500

    Added my name to the executable

---
+++ input2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# read the name of the user and print hello
+
+echo "Hello Albert! Type in anything!"
+read input
+echo "You typed, $input"
+
+# single quotes prevent the expansion of the variable
+
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
