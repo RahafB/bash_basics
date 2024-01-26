648841aff188278e18dc590cff27474d69265059
MiguelL1304 <mleon6@ggc.edu>
2024-01-25 21:00:40 -0500

    Added a cat song

--- 07-beer.sh
+++ 07-beer.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 
-echo "Let's sing a beer song"
-echo "How many bottles?"
-read count
+echo "Let's sing a Cats song"
+echo "How many cats?"
+read ducks
+
+count=$ducks
 
 while [ $count -ge 0 ]; do
+  echo ""
+  echo "$ducks little cat went swimming one day"
+  echo "Over the hills and far away"
+  echo "The little cat said: meow, meow, meow, meow"
+
+  if [ $count -ge $ducks ]; then
+      echo "And $count little cats came back"
   if [ $count -ge 2 ]; then
-      echo "$count bottles of beer on the wall, $count bottles of beer"
-      echo "Take one down pass it around"
+      echo "And only $count little cats came back"
   elif [ $count -eq 1 ]; then
-      echo "$count bottle of beer on the wall, $count bottles of beer"
-      echo "Take one down pass it around"
+      echo "And only $count little cat came back"
   else
-      echo "no more bottles of beer on the wall"
+      echo "And then no more little cats came back"
   fi
-  
+
   # the following statement is equivalent to: let "count=count-1"
+
   ((count = count - 1))
 
 done
 
-# exercise: implement another counting song (such as 12 days of Christmas) 
-# using loops and if statements.
+# exercise: implement another counting song (such as 12 days of Christmas)
+# using loops and if statements. what is wrong
