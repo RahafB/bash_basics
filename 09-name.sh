8f66087a6d9579f8fa9f16e932e96a25eae19498
jportillo5 <jtsportillo@gmail.com>
2024-01-28 18:04:24 -0500

    09-name.sh changed

--- 09-name.sh
+++ 09-name.sh
@@ -2,11 +2,18 @@
 
 # look up ip addresses of various search engines
 
-servers="yahoo.com google.com dogpile.com wolframalpha.com"
+#servers="yahoo.com google.com dogpile.com wolframalpha.com"
+
+#for server in $servers; do
+    #nslookup $server
+    #echo "----------------------------"
+#done
+
+servers="youtube.com tetr.io wikipedia.org"
 
 for server in $servers; do
-    nslookup $server
-    echo "----------------------------"
+	ping $server
+	echo "----------------------------"
 done
 
 # exercise: Change the list of servers and also the 
