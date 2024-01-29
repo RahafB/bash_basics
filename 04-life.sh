2514ddb70b1448f44689488ae0ae665a112b5c4f
Albert <albert.austiniv@gmail.com>
2024-01-28 23:28:01 -0500

    did 04 exercise

--- 04-life.sh
+++ 04-life.sh
@@ -1,16 +1,25 @@
 
-echo "What is the meaning of life?"
-read meaning
+#echo "What is the meaning of life?"
+#read meaning
 
 
-if [ "$meaning" -eq 42 ]; then
-   echo "Yes!, That is the meaning of life!"
-else
-   echo "Awww... You don't know the meaning of life"
-fi
+#if [ "$meaning" -eq 42 ]; then
+#   echo "Yes!, That is the meaning of life!"
+#else
+#   echo "Awww... You don't know the meaning of life"
+#fi
 
 #  here are some other arithemetic comparison operators
 # -eq -ne -gt -ge -lt -le
 
 # exercise: write a script that prints whether it is 
 # morning or not
+
+echo "Is it bright outside?"
+read light
+
+if [ "$meaning" == "yes" ]; then
+	echo "It's definitely morning"
+else
+	echo "It's definitely night"
+fi
