123d7fe7aa0ff75aee8f080b1ac1f697de4b1a8a
jasonurquilla <jurquilla@ggc.edu>
2024-01-28 21:03:58 -0500

    added second version of 05-grade.sh

--- 05-grade.sh
+++ 05-grade.sh
@@ -19,3 +19,19 @@ fi
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
 # everything else
+
+echo "What is the temperature outside?"
+echo "Please give a numeric answer"
+read temperature
+
+if [ $temperature -ge 70 ]; then
+   echo "Its hot"
+elif [ $temperature -ge 60 ]; then
+   echo "Its okay"
+elif [ $temperature -ge 40 ]; then
+   echo "It's chilly"
+elif [ $temperature -ge 0]; then
+	echo "It is quite cold"
+else
+   echo "temperature below 0 brrrrr"
+fi
